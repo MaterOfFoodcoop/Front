d7782896d7856db09ebfd9bff4b18472bf413250
Wickhye <jihyesim170@gmail.com>
2023-11-03 12:22:46 +0900

    chore: styled-component 세팅

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,7 +1,9 @@
+import AppLayout from "@/layouts/AppLayout";
+
 export default function Home() {
   return (
-    <div>
-
-    </div>
+    <AppLayout>
+      하하하 나는 홈이다
+    </AppLayout>
   )
 }
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,2 +1,2 @@
 // component exports
-export { Card } from "./card";
+export { color } from './src/color';
