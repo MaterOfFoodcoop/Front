e923ab30ad6648da0b83734fc3a99fb120d238cf
Wickhye <jihyesim170@gmail.com>
2023-12-11 01:17:08 +0900

    chore: 상품 리스트

--- packages/api/instance/instance.ts
+++ packages/api/instance/instance.ts
@@ -6,17 +6,13 @@ export const instance = axios.create({
   timeout: 10000,
 });
 
-// 요청 인터셉터 추가하기
-axios.interceptors.request.use(
-  function (config) {
-    // 요청이 전달되기 전에 작업 수행
-    return config;
-  },
-  function (error) {
-    // 요청 오류가 있는 작업 수행
-    return Promise.reject(error);
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+  return config;
+});
 
 // 응답 인터셉터 추가하기
 axios.interceptors.response.use(
--- packages/ui/src/components/Header/AdminLoginModal/AdminLoginModal.tsx
+++ packages/ui/src/components/Header/AdminLoginModal/AdminLoginModal.tsx
@@ -1,102 +1,88 @@
-import { useContext, useState } from 'react';
-import styled from 'styled-components';
+import { useState } from "react";
+import styled from "styled-components";
 
-import { color, font } from 'ui/styles';
-import { WriteButton } from 'ui/components'
-import { HamburgerIcon, CloseIcon } from 'ui/icon';
+import { color, font } from "ui/styles";
+import { WriteButton } from "ui/components";
+import { HamburgerIcon, CloseIcon } from "ui/icon";
+import { useLoginMutation } from "apis/auth/mutation";
+import { useRouter } from "next/navigation";
 
 interface AdminLoginProps {
-  isOpen: boolean,
+  isOpen: boolean;
   onClose: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 function AdminLoginModal({ isOpen, onClose }: AdminLoginProps) {
-
-  const [userId, setUserId] = useState("");
-  const [userPassword, setUserPassword] = useState(""); 
-
-  // 아이디, 비밀번호 저장
-  const loginHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { id, value } = e.target;
-    if (id === 'userid') {
-      setUserId(value);
-    } else if (id === 'password') {
-      setUserPassword(value);
-    }
-  }
-  
-  // Admin 계정과 일치한다면 로그인
-  const loginClickHandler = async () => {
-    const id = userId;
-    const password = userPassword;
-
-      // 백엔드 로그인 요청
-      await fetch("http://localhost:3001/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          id,
-          password,
-        }),
-      })
-        .then((res) => res.json())
-        .then((res) => console.log(res));
-  }; 
+  const router = useRouter();
+  const [loginUserData, setLoginUserData] = useState({
+    email: "",
+    password: "",
+  });
+  const loginMutate = useLoginMutation(loginUserData);
+  const handleLoginUserData = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setLoginUserData({ ...loginUserData, [name]: value });
+  };
+  const loginClickHandler = () => {
+    loginMutate.mutate();
+    router.push("/");
+  };
 
   return (
     <>
-    {isOpen ? (
-      <Container>
-        <ModalBox>
-          <IconArea>
-            <HamburgerIcon />
-          </IconArea>
+      {isOpen ? (
+        <Container>
+          <ModalBox>
+            <IconArea>
+              <HamburgerIcon />
+            </IconArea>
 
-          <Content>
+            <Content>
               <CloseBtn onClick={onClose}>
                 <CloseIcon />
               </CloseBtn>
 
-            <TitleArea>
-              <Title>와라! 매점</Title>
-              <Subtitle>관리자 계정으로 로그인</Subtitle>
-            </TitleArea>
+              <TitleArea>
+                <Title>와라! 매점</Title>
+                <Subtitle>관리자 계정으로 로그인</Subtitle>
+              </TitleArea>
 
-            <InfoForm>
-              <InfoInputBox>
-                <InfoInputLabel htmlFor="userid">아이디</InfoInputLabel>
-                  <InfoInput 
-                    type="text" 
-                    id="userid" 
-                    name="userid" 
-                    onChange={loginHandler}
+              <InfoForm>
+                <InfoInputBox>
+                  <InfoInputLabel htmlFor="userid">아이디</InfoInputLabel>
+                  <InfoInput
+                    type="text"
+                    id="email"
+                    name="email"
+                    onChange={handleLoginUserData}
                   />
-              </InfoInputBox>
-              <InfoInputBox>
-                <InfoInputLabel>비밀번호</InfoInputLabel>
-                  <InfoInput 
-                    type="password" 
-                    id="password" 
-                    name="password" 
-                    onChange={loginHandler}
+                </InfoInputBox>
+                <InfoInputBox>
+                  <InfoInputLabel>비밀번호</InfoInputLabel>
+                  <InfoInput
+                    type="password"
+                    id="password"
+                    name="password"
+                    onChange={handleLoginUserData}
                   />
-              </InfoInputBox>
+                </InfoInputBox>
 
-              <ButtonArea>
-                <WriteButton style={{ backgroundColor: '#f9f9f9' }} onClick={loginClickHandler}>
-                  로그인
-                </WriteButton>
-              </ButtonArea>
-
-            </InfoForm>
-          </Content>
-        </ModalBox>
-      </Container>
-    ) : null}
-  </>
-)};
+                <ButtonArea>
+                  <WriteButton
+                    style={{ backgroundColor: "#f9f9f9" }}
+                    onClick={loginClickHandler}
+                  >
+                    로그인
+                  </WriteButton>
+                </ButtonArea>
+              </InfoForm>
+            </Content>
+          </ModalBox>
+        </Container>
+      ) : null}
+    </>
+  );
+}
 
 export default AdminLoginModal;
 
@@ -105,7 +91,7 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
 
-  background: rgba(0, 0, 0, 0.10);
+  background: rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(2px);
   z-index: 250;
 
@@ -120,7 +106,7 @@ const Container = styled.div`
 const ModalBox = styled.div`
   display: flex;
   flex-direction: row;
-`
+`;
 
 const IconArea = styled.div`
   width: 23.6rem;
@@ -145,7 +131,7 @@ const Content = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const CloseBtn = styled.button`
   width: 1.6rem;
@@ -160,7 +146,7 @@ const TitleArea = styled.div`
   gap: 14px;
   align-self: flex-start;
   margin: 1rem 0rem 3.3rem 3.88rem;
-`
+`;
 
 const Title = styled.p`
   ${font.Header1};
@@ -193,7 +179,7 @@ const InfoInput = styled.input`
   width: 31rem;
   height: 3.8rem;
   font-size: 1rem;
-  
+
   background-color: ${color.gray50};
   outline: none;
   border: none;
@@ -201,8 +187,8 @@ const InfoInput = styled.input`
   padding: 16px 20px;
 
   &::placeholder {
-    ${font['Placeholder']};
-    color: ${color.gray400}    
+    ${font["Placeholder"]};
+    color: ${color.gray400};
   }
 `;
 
