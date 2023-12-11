8f4e234424b9dc2b1eb9f161fb65745088a2104e
원설아 <seolalny@gmail.com>
2023-12-11 11:23:36 +0900

    chore(client): postAnswer export

--- packages/api/qna/answer/api.ts
+++ packages/api/qna/answer/api.ts
@@ -1,6 +1,6 @@
 import { instance } from "../../instance/instance";
 
-const postAnswer = async ({ answer, id }) => {
+export const postAnswer = async ({ answer, id }) => {
     const response = await instance.post('/api/answer', { answer, id });
     return response.data;
 };
